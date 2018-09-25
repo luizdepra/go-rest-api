175bca1bfc8d6ea99369f1ce55d7e26c07a2880b
Luiz de Prá <luiz.pra@olist.com>
2018-09-25 11:56:32 -0300

    Add routes

--- go.mod
+++ go.mod
@@ -1,1 +1,7 @@
 module github.com/luizdepra/go-rest-api
+
+require (
+	github.com/go-playground/form v3.1.3+incompatible // indirect
+	github.com/go-playground/pure v4.1.1+incompatible
+	github.com/mdempsky/gocode v0.0.0-20180727200127-00e7f5ac290a // indirect
+)
--- main.go
+++ main.go
@@ -1,15 +1,48 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/go-playground/pure"
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello world!")
-	})
+	p := pure.New()
 
-	log.Fatal(http.ListenAndServe(":8000", nil))
+	p.Get("/", rootHandler)
+
+	todos := p.Group("/todos")
+	todos.Get("", listTODOsHandler)
+	todos.Post("/", createTodoHandler)
+	todos.Get("/:id", getTODOHandler)
+	todos.Put("/:id", updateTODOHandler)
+	todos.Patch("/:id", updateTODOHandler)
+	todos.Delete("/:id", deleteTODOHandler)
+
+	log.Fatal(http.ListenAndServe(":8000", p.Serve()))
+}
+
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello world!"))
+}
+
+func listTODOsHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Listed!"))
+}
+
+func createTodoHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Created!"))
+}
+
+func getTODOHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Got!"))
+}
+
+func updateTODOHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Updated!"))
+}
+
+func deleteTODOHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Deleted!"))
 }
